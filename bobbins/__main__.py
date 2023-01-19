7b91189d2f86d03153c4aaa99d1f8da83d31ddb9
Zech Zimmerman <hi@zech.codes>
2023-01-19 18:58:50 -0500

    Add missing guild members intent

--- bobbins/__main__.py
+++ bobbins/__main__.py
@@ -1,9 +1,13 @@
+import hikari
+
 import bobbins.bot
 import bobbins.plugin_posts
 import bobbins.plugin_recent_posts
 
-
-bot = bobbins.bot.Bot(help_slash_command=True)
+bot = bobbins.bot.Bot(
+    help_slash_command=True,
+    intents=hikari.Intents.ALL_UNPRIVILEGED | hikari.Intents.GUILD_MEMBERS,
+)
 bot.add_plugin(bobbins.plugin_posts.post_plugin)
 bot.add_plugin(bobbins.plugin_recent_posts.recent_posts_plugin)
 bot.run()
